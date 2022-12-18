26c7dd4595aaeb9284a58900a6faf4ffb5b6bc2e
芝华塔尼欧 <jonathanleelx@gmail.com>
2022-12-18 12:10:40 +0000

    test: add react project test case

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -205,3 +205,87 @@ it("simple vue project", async () => {
   const notExist = graph.get("not-exist");
   expect(notExist).toBeUndefined();
 });
+
+it("simple react project", async () => {
+  moduleMap = {
+    "index.js": {
+      source: `
+        import React from 'react'
+        import { render } from 'react-dom'
+        import App from 'App.js'
+
+        render('<App />', '#app')
+      `,
+    },
+    "App.js": {
+      source: `
+        import React, { useState } from 'react'
+        import HelloWorld from 'HelloWorld'
+
+        export default function App() {
+          const [count, setCount] = useState(0)
+          return (
+            <>
+              <HelloWorld></HelloWorld>
+              <span>{count}</span>
+              <button onClick={() => setCount(count + 1)}>inc</button>
+            </>
+          )
+        }
+      `,
+    },
+    HelloWorld: {
+      source: `
+        import React from 'react'
+
+        export default function HelloWorld() {
+          return (
+            <p>
+              HelloWorld
+            </p>
+          )
+        }
+      `,
+    },
+    react: {
+      source: `
+        const React = {}
+
+        export default React
+
+        export const useState = (val) => {
+          let _val = val
+          return [_val, (newVal) => _val = newVal]
+        }
+      `,
+    },
+    ["react-dom"]: {
+      source: `
+        export const render = (jsx, el) => {}
+      `,
+    },
+  };
+
+  baseConfig.plugins.push({
+    parse(id, code) {
+      return parseSync(code, {
+        jsx: true,
+        syntax: "ecmascript",
+      });
+    },
+  });
+
+  const graph = await createModuleGraphFromEntry("index.js", baseConfig);
+
+  const app = graph.get("App.js");
+  const index = graph.get("index.js");
+  const hello = graph.get("HelloWorld");
+  const reactDom = graph.get("react-dom");
+  const react = graph.get("react");
+
+  expect(graph.size).toBeGreaterThan(0);
+  expect(hello?.deps.has(app!)).toBeTruthy();
+  expect(reactDom?.deps.has(index!)).toBeTruthy();
+  expect(react?.deps.has(hello!)).toBeTruthy();
+  expect(react?.deps.has(app!)).toBeTruthy();
+});
