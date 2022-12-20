329650ff8274a162686461e7ef59eb08512d71e1
jonathanleelx <lixiang@14@wps.cn>
2022-12-20 12:58:15 +0800

    feat: default parser

--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,35 @@
 import { Module, parseSync } from "@swc/core";
-import { Config } from "./config";
+import { Config, UserPlugin } from "./config";
+
+const defaultParse: UserPlugin["parse"] = (
+  id: string,
+  code: string
+): Module | null => {
+  // 只处理.jsx?,.tsx?的文件
+  if (!/[jt]sx?$/.test(id)) return null;
+  const isTS = id.endsWith(".ts");
+  const isTSX = id.endsWith(".tsx");
+  const isJSX = id.endsWith(".jsx");
+  let ast: Module | null = null;
+
+  try {
+    ast =
+      ast === null
+        ? parseSync(code, {
+            syntax: isTS ? "typescript" : "ecmascript",
+            decorators: true,
+            dynamicImport: true,
+            tsx: isTSX,
+            jsx: isJSX,
+          })
+        : ast;
+    return ast;
+  } catch (error) {
+    console.error("parse id " + id + " failed");
+    // throw error;
+    return null;
+  }
+};
 
 export async function doParse(
   id: string,
@@ -25,25 +55,5 @@ export async function doParse(
     }
   }
 
-  const isTS = id.endsWith(".ts");
-  const isTSX = id.endsWith(".tsx");
-  const isJSX = id.endsWith(".jsx");
-
-  try {
-    ast =
-      ast === null
-        ? parseSync(code, {
-            syntax: isTS ? "typescript" : "ecmascript",
-            decorators: true,
-            dynamicImport: true,
-            tsx: isTSX,
-            jsx: isJSX,
-          })
-        : ast;
-    return ast;
-  } catch (error) {
-    console.error("parse id " + id + " failed");
-    // throw error;
-    return null;
-  }
+  return defaultParse!(id, code);
 }
